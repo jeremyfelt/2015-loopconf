0f995522d8cb9b007e5a67d0af3d92565efc6952
Jeremy Felt <jeremy.felt@gmail.com>
2015-04-18 15:04:58 -0700

    Continue setup of initial structure to support slides directory

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+/* global module:false */
+module.exports = function(grunt) {
+	var port = grunt.option('port') || 8000;
+	// Project configuration
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		connect: {
+			server: {
+				options: {
+					port: port,
+					base: '.',
+                    livereload: true,
+                    open: true
+				}
+			}
+		},
+
+		watch: {
+            html: {
+                files: [ 'index.html']
+            }
+		}
+
+	});
+
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-contrib-connect' );
+
+	// Serve presentation locally
+	grunt.registerTask( 'serve', [ 'connect', 'watch' ] );
+};
